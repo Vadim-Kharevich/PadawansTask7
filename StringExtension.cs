be34e065cf3aebaf65bf1b667aa652aba302d7b7
Vadim-Kharevich <xarevich99@gmail.com>
2019-02-16 22:54:53 +0300

    Update StringExtension.cs

--- StringExtension.cs
+++ StringExtension.cs
@@ -6,6 +6,10 @@ namespace PadawansTask7
     {
         public static void OrderStringsByLength(string[] array)
         {
+            if (array == null)
+            {
+                throw new ArgumentNullException();
+            }
             for (int i = 0; i < array.Length - 1; i++)
             {
                 if (array[i].Length > array[i + 1].Length)
@@ -14,6 +18,10 @@ namespace PadawansTask7
                     array[i + 1] = array[i];
                     array[i] = str;
                     i = -1;
+                }
+                if (array[i] == null)
+                {
+                    throw new ArgumentNullException();
                 }
             }
         }
